a96e5d5d82b47a22767e2c6fa47e62be51bf39f6
Fella Kettani <fellakettani@gmail.com>
2023-08-13 01:07:27 +0100

    finished assignement

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+function findMatching (names, strings){
+    const matchingDrivers = names.filter(name =>
+        name.toLowerCase().includes(strings.toLowerCase())
+    );
+
+    return matchingDrivers;
+}
+
+const drivers = ["Allan", "David", "Rick", "John", "Bobby", "Jake", "Nick"];
+
+console.log(findMatching(drivers, "bobby"));
+
+
+function fuzzyMatch(driverNames, letter) {
+    return driverNames.filter(driverName =>
+        driverName.toLowerCase().startsWith(letter.toLowerCase())
+    );
+}
+
+const letter = "J";
+const matchingDrivers = fuzzyMatch(drivers, letter);
+console.log(matchingDrivers);
+
+
+
+
+
+  function matchName (bames, strings){
+    const matchingDrivers = bames.filter(bames =>
+        bames.name.toLowerCase().includes(strings.toLowerCase())
+    );
+
+    return matchingDrivers;
+}
+
+const newDrivers = [
+    { name: "John", hometown: "New York" },
+    { name: "Alice", hometown: "Los Angeles" },
+    { name: "Bob", hometown: "Chicago" },
+    { name: "John", hometown: "Miami" }
+  ];
+
+  console.log(matchName(newDrivers, newDrivers.name="alice"));
